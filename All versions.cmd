767efa1a66d7f94349f3fdec9bc7d1bf7fb7a17b
zLuxlan <117448811+zLuxlan@users.noreply.github.com>
2023-01-15 18:52:45 +0100

    Add files via upload

---
+++ All versions.cmd
@@ -0,0 +1,176 @@
+@echo off 
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+echo msgbox "By Luclan" >%temp%/msg.vbs
+cls
+start %temp%/msg.vbs
+cls
+TITLE Crack Windows All versions
+goto main
+:main
+echo.
+echo.
+echo.
+echo.                                                        KMS
+echo.
+echo.                                                        Installer                                           
+echo.                                    
+echo.                                      
+echo.
+echo.
+echo.
+echo.
+call :ColorText 06 "                                  [ "
+call :ColorText F " 1 "
+call :ColorText 06 " ] " 
+call :ColorText F " Win Pro "
+call :ColorText 06 "                                  [ "
+call :ColorText F " 2 "
+call :ColorText 06 " ] " 
+call :ColorText F " Familly "
+echo.
+echo.
+echo.
+call :ColorText 06 "                                  [ "
+call :ColorText F " 3 "
+call :ColorText 06 " ] " 
+call :ColorText F " Profetional "
+call :ColorText 06 "                                  [ "
+call :ColorText F " 4 "
+call :ColorText 06 " ] " 
+call :ColorText F " Profetional N "
+echo.
+echo.
+echo.
+call :ColorText 06 "                                  [ "
+call :ColorText F " 5 "
+call :ColorText 06 " ] " 
+call :ColorText F " Entreprise "
+call :ColorText 06 "                                  [ "
+call :ColorText F " 6 "
+call :ColorText 06 " ] " 
+call :ColorText F " Entreprise N "
+echo.
+echo.
+echo.
+call :ColorText 06 "                                  [ "
+call :ColorText F " 7 "
+call :ColorText 06 " ] " 
+call :ColorText F " Home "                                  [ "
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p choice="Select a corresponding number to what you'd like > "
+if /i "%choice%"=="1" goto KMSPro
+if /i "%choice%"=="2" goto KMSFamilly
+if /i "%choice%"=="3" goto KMSProfetional 
+if /i "%choice%"=="4" goto KMSProfetionalN
+if /i "%choice%"=="5" goto KMSEnterprise
+if /i "%choice%"=="6" goto KMSEnterpriseN
+if /i "%choice%"=="7" goto KMSHome
+
+
+
+:ColorText
+@echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
+
+:KMSPro
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+
+:KMSFamilly
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk 334NH-RXG76-64THK-C7CKG-D3VPT
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+:KMSProfetional
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+:KMSProfetionalN
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+:KMSEnterprise
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+:KMSEnterpriseN
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
+:KMSHome
+cls
+@echo off
+color 06
+echo. free activator by luclan
+slmgr /upk
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo. done
+cls
+goto main
+
